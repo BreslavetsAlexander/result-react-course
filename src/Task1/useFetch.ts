9878b5ebe7f805caa03df9845c660f36e1cae8c2
BreslavetsAlexander <breslavets28101@gmail.com>
2024-10-20 21:35:33 +0300

    add utils

--- src/Task1/useFetch.ts
+++ src/Task1/useFetch.ts
@@ -1,13 +1,6 @@
 import { useEffect, useState } from 'react';
 import { RefetchParams } from './types';
-
-export const prepareParams = (params: RefetchParams['params']): string => {
-  const list = Object.keys(params).reduce<string[]>((result, key) => {
-    return [...result, `${key}=${encodeURIComponent(params[key])}`];
-  }, []);
-
-  return `?${list.join('&')}`;
-};
+import { prepareParams } from './utils';
 
 export const useFetch = <Data, Error>(url: string) => {
   const [data, setData] = useState<Data | null>(null);
