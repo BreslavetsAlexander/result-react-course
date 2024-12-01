03c7f07ab528bc8d3ea0b7205d420d6c7366b92e
BreslavetsAlexander <breslavets28101@gmail.com>
2024-12-01 17:43:38 +0300

    fix

--- src/Task3/useHover.ts
+++ src/Task3/useHover.ts
@@ -1,17 +1,17 @@
-import { useEffect, useRef, useState } from 'react';
+import { useEffect, useRef, useState, useCallback } from 'react';
 
 export const useHover = <TElement extends HTMLElement>() => {
   const [hovered, setHovered] = useState<boolean>(false);
   const ref = useRef<TElement>(null);
 
   useEffect(() => {
-    const onMouseEnter = () => {
+    const onMouseEnter = useCallback(() => {
       setHovered(true);
-    };
+    }, []);
 
-    const onMouseLeave = () => {
+    const onMouseLeave = useCallback(() => {
       setHovered(false);
-    };
+    }, []);
 
     ref.current?.addEventListener('mouseenter', onMouseEnter);
     ref.current?.addEventListener('mouseleave', onMouseLeave);
